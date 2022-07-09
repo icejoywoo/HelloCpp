0a3d56eb67bbe1f13fb35a860f66285f75de8824
icejoywoo <icejoywoo@gmail.com>
2022-07-09 22:52:07 +0800

    update

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ HelloCpp
 ========
 
 demos for learning cpp
+
+
+``` bash
+git submodule add https://github.com/google/googletest.git cpp-project/third_party/googletest
+git submodule add https://github.com/xtensor-stack/xsimd.git cpp-project/third_party/xsimd
+
+git submodule sync --recursive
+git submodule update --init --recursive
+```
--- cpp-project/basic/lambda_test.cc
+++ cpp-project/basic/lambda_test.cc
@@ -19,20 +19,21 @@ protected:
     }
 };
 
-TEST_F(LambdaTest, BasicTest)
-{
+TEST_F(LambdaTest, BasicTest) {
     {
         int x = 5;
-        auto foo = [r = x + 1]{return r;};
+        auto foo = [r = x + 1] { return r; };
         ASSERT_EQ(6, foo());
     }
 
     {
         std::string x = "hello c++ ";
-        auto foo = [x = std::move(x)]{return x + "world";};
+        auto foo = [x = std::move(x)] { return x + "world"; };
         ASSERT_EQ("hello c++ world", foo());
     }
+}
 
+TEST_F(LambdaTest, ThisTest) {
     class Work
     {
     private:
@@ -42,8 +43,8 @@ TEST_F(LambdaTest, BasicTest)
         std::future<int> spawn()
         {
 
-//            return std::async([*this]() -> int {return value;});
-            return std::async([=, tmp = *this]() -> int {return tmp.value;});
+            return std::async([=, *this]() -> int {return value;}); // c++ 17
+//            return std::async([=, tmp = *this]() -> int {return tmp.value;});
 //            return std::async([=]() -> int {return value;}); // undefined behavior
         }
     };
@@ -56,4 +57,15 @@ TEST_F(LambdaTest, BasicTest)
     auto f = foo();
     f.wait();
     ASSERT_EQ(42, f.get());
+}
+
+TEST_F(LambdaTest, AutoTest) {
+    auto foo = [](auto a) { return a; };
+    int three = foo(3);
+    ASSERT_EQ(3, three);
+    char const* hello = foo("hello");
+    ASSERT_EQ("hello", hello);
+
+    auto greater = [](auto x, auto y) { return x > y; };
+//    std::map<std::string, int, decltype(greater)> mymap; // only for c++ 20
 }
