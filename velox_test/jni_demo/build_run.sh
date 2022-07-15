6dd211f40e03996642cbb1ce8daec0832eac91ef
icejoywoo <icejoywoo@gmail.com>
2022-07-15 21:34:05 +0800

    update

--- velox_test/jni_demo/build_run.sh
+++ velox_test/jni_demo/build_run.sh
@@ -6,11 +6,12 @@ set -e
 CURRENT_DIR=$(cd `dirname $0` && pwd)
 PROJECT_DIR=${CURRENT_DIR}/../
 JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+JAVA_DEBUG="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000"
 
 #${JAVA_HOME}/bin/javac -h ${PROJECT_DIR}/jni JniDemo.java
-${JAVA_HOME}/bin/javac -h ${CURRENT_DIR} JniDemo.java
+${JAVA_HOME}/bin/javac -g -h ${CURRENT_DIR} JniDemo.java
 
 #g++ -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin JniDemo.cc -o JniDemo.o
 #g++ -dynamiclib -o libjnidemo.dylib JniDemo.o -lc
 
-java -cp . -Djava.library.path=${PROJECT_DIR}/cmake-build-debug/jni_demo/ JniDemo
+java ${JAVA_DEBUG} -cp . -Djava.library.path=${PROJECT_DIR}/cmake-build-debug/jni_demo/ JniDemo
